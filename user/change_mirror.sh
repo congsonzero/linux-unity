b95029d9111968cf623160ebab70ed4772f3be8e
congsonzero <zerosonnguyen@gmail.com>
2020-10-22 12:48:35 +0700

    update

---
+++ user/change_mirror.sh
@@ -0,0 +1,2 @@
+#sudo nano /etc/apt/sources.list
+#change deb
--- user/create_user.sh
+++ user/create_user.sh
@@ -2,4 +2,12 @@
 #New password(Tao pass)
 #Retype new UNIX password:
 # usermod -aG sudo test_user
-
+#get all group user 
+    #cat /etc/group | grep sudo(grep lọc )
+    #getent group
+#Linux List All Group Names
+    #cut -d: -f1 /etc/group
+    #getent group | cut -d: -f1 
+#Count of All the Linux Groups
+    #cat /etc/group | grep -c ""
+    #getent group | grep -c ""
